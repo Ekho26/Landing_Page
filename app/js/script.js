27c60fee1cacf9c57d0664660be0b9a21971c3da
Ekho26 <munteanu.ionut1991@gmail.com>
2021-02-23 15:54:30 +0200

    Animated Hamburger

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,15 @@
-console.log('Hello world')
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+header.addEventListener("click", function () {
+  console.log("click header");
+  if (header.classList.contains("open")) {
+    header.classList.remove("open");
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
+  } else {
+    header.classList.add("open");
+    overlay.classList.remove("fade-out")
+    overlay.classList.add("fade-in")
+  }
+});
